5972232134525e16bc53144ade5a2b8e75b9799d
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-01-31 13:46:01 +0300

    define the bottom navigation

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Icon(Icons.home, size: 150),
+    Icon(Icons.stacked_line_chart_outlined, size: 150),
+    Icon(Icons.add, size: 150),
+    Icon(Icons.notifications, size: 150),
+    Icon(Icons.settings, size: 150)
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,8 +17,29 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
       ),
       body: Center(
-        child: Text('Welcome to the Home Page!'),
+        child: _pages.elementAt(_selectedIndex)
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 30,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon:Icon( Icons.home, color: Colors.black,), label: 'Today'),
+          BottomNavigationBarItem(icon: Icon(Icons.stacked_line_chart_outlined, color: Colors.black,), label: 'Analytics'),
+          BottomNavigationBarItem(icon: Icon(Icons.add, color: Colors.amber,), label: 'New'),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications, color: Colors.black,), label: 'Notifications'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings, color: Colors.black), label: 'Settings'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+
     );
+  }
+  void _onItemTapped(int index){
+    setState(()=>_selectedIndex=index);
+
   }
 }
