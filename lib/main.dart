8419d50cdbf1325ac7170b963c9ed7ba1c7a3978
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-01-31 10:54:02 +0300

    implement ios configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'firebase_options.dart';
 import 'package:habit_tracker_plp/pages/sign_up_page.dart';
 import 'package:habit_tracker_plp/pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
